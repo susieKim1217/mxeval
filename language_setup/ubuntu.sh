4835dc926f8c5fef15f3849cc5c1cab8ca4a1e3c
susie <87310953+susieKim1217@users.noreply.github.com>
2024-03-12 10:30:37 +0900

    Update ubuntu.sh

--- language_setup/ubuntu.sh
+++ language_setup/ubuntu.sh
@@ -31,7 +31,7 @@ source ~/.bashrc
 sudo apt install -y npm
 sudo nvm install 16.10.0
 node -e "console.log('Running Node.js ' + process.version)"
-sudo npm i -g npm
+sudo npm i -g npm@8.19.4
 sudo npm install -g lodash
 sudo npm i --save lodash
 
