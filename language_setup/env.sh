1e623534a62564f921555019bb8a94c462855312
susie <87310953+susieKim1217@users.noreply.github.com>
2024-03-14 16:06:43 +0900

    Update env.sh

--- language_setup/env.sh
+++ language_setup/env.sh
@@ -7,3 +7,11 @@ printf "%100s" " " | tr ' ' '-'
 echo ""
 apt install -y sudo pip vim npm wget curl
 pip install tqdm fire numpy gunicorn flask requests psutil cx_Oracle inquirer
+
+printf "%100s" " " | tr ' ' '-'
+echo ""
+echo "setting up JavaScript "
+printf "%100s" " " | tr ' ' '-'
+echo ""
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
